1911fa2ece9d9521e7831bab00f23fb3845ab5db
orangeSuner <736123630@qq.com>
2023-08-31 11:40:07 +0800

    参数调整

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
 import * as THREE from 'three'
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
 const render = new THREE.WebGLRenderer()
 
-camera.position.set(1, 1, 1)
+camera.position.set(20, 20, 100)
 scene.add(camera)
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: '#ff6800'})
+const cubeGeometry = new THREE.BoxGeometry(5, 5, 5)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: '#ff6800',
+  transparent: true,
+  opacity: 0.5
+})
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+cube.position.set(20,20,20)
 scene.add(cube)
+
+const axesHelper = new THREE.AxesHelper(200)
+scene.add(axesHelper)
 
 render.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(render.domElement)
